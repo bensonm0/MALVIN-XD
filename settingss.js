a1041188f7508f5b7a87a8a7c854dd90311b34fe
bensonm0 <143939851+bensonm0@users.noreply.github.com>
2025-08-22 10:36:25 +0000

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,8 +1,8 @@
 
 module.exports = {
-  SESSION_ID: "",  // add your Session Id here
+  SESSION_ID: "malvin~UnFjgLQI#KCvqz2OxzG2eH9qJQEu7bzbT4VecmGZ5t5CXrvmNxUg",  // add your Session Id here
   
-  OWNER_NUMBER: "263714757857", // put your phone number here
+  OWNER_NUMBER: "22870587219", // put your phone number here
   
   PREFIX: ".", // prefix (e.g., ., /, !, *)
   
